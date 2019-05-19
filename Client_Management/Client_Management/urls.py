6cc91d082ca0f4708502095dcabec0f1cca5c9b3
Eric Gil <ericbgil@gmail.com>
2019-05-19 20:04:01 -0300

    New files added to project (Clients and home)

--- Client_Management/Client_Management/urls.py
+++ Client_Management/Client_Management/urls.py
@@ -14,8 +14,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from Clients import urls as Clients_URLS
+from django.conf import settings
+from home import urls as home_URLS
+from django.conf.urls.static import static
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
+    path('', include(home_URLS)),
+	path("Clients/", include(Clients_URLS)),
+	path('login/', auth_views.LoginView.as_view(), name='login'),
     path('admin/', admin.site.urls),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
