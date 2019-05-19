b426f0c1cf35c98a22e024a2def2ded8331d123d
Eric Gil <ericbgil@gmail.com>
2019-05-19 15:51:56 -0300

    Initial commits, just the empty Django project and the Python 3 virtual environment

---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/eric/Documents/Python_Development/Projeto_Django_Heroku/Django_Heroku_Project/venv/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
